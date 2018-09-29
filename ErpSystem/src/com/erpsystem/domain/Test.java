d18c0b45a0ba9fca14fc70ce6116db92cd02b933
MrLiu <as920579353@163.com>
2018-09-29 15:45:55 +0800

    建立包结构

---
+++ ErpSystem/src/com/erpsystem/domain/Test.java
@@ -0,0 +1,14 @@
+package com.erpsystem.domain;
+
+/**
+ * 
+ * @function   测试
+ * @author     极客空
+ * @date       2018年9月29日 下午3:40:00
+ * @copyright  MR.Liu
+ * @address    成都
+ *
+ */
+public class Test {
+
+}
---
+++ ErpSystem/src/com/erpsystem/web/Test.java
@@ -0,0 +1,14 @@
+package com.erpsystem.web;
+
+/**
+ * 
+ * @function   测试
+ * @author     极客空
+ * @date       2018年9月29日 下午3:44:02
+ * @copyright  MR.Liu
+ * @address    成都
+ *
+ */
+public class Test {
+
+}
